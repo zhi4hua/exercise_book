87eed7de70520b971f6c9168739783d496817877
zhi4hua <zhi4hua@yeah.net>
2015-11-21 14:36:05 +0800

    Update showValue.js

--- js/showValue.js
+++ js/showValue.js
@@ -5,12 +5,14 @@ function showValue() {
 }
 
 // 类方法，功能为－追加变量致 Body 标签当中
-showValue.prototype.app = function(value1) {
+showValue.prototype.app = function(value1, showIndex) {
   this.myBody = document.getElementsByTagName('body')[0];
   var myHr = document.createElement('hr');
   var myP = document.createElement('p');
 
-  myP.innerHTML = ++this.total + '&#9' + value1;
+  if(showTndex)
+    myP.innerHTML = ++this.total;
+  myP.innerHTML = myP.innerHTML + '&#9' + value1;
   this.myBody.appendChild(myHr);
   this.myBody.appendChild(myP);
   if (this.total % 2 == 1) {
