4e6543d8d58f2611d2acd975c5bd294c06fa645c
zhi4hua <zhi4hua@yeah.net>
2015-12-04 09:44:20 +0800

    扩展功能

--- js/showValue.js
+++ js/showValue.js
@@ -4,18 +4,37 @@ function showValue() {
   this.total = 0;
 }
 
-// 类方法，功能是将参数 value1 值显示在页面.
-showValue.prototype.app = function(value1) {
+// 类方法，功能为－追加变量致 Body 标签当中
+showValue.prototype.app = function(value1, color, index) {
   this.myBody = document.getElementsByTagName('body')[0];
   var myHr = document.createElement('hr');
   var myP = document.createElement('p');
 
-  // if(showTndex == true)
-  //   myP.innerHTML = ++this.total;
-  myP.innerHTML = ++this.total + '&#9' + value1;
+  if (typeof(index) == "number")
+    this.total = index;
+  if (index == undefined)
+    myP.innerHTML = ++this.total + '&#9' + value1;
+  else if (index == false)
+    myP.innerHTML = value1;
+
   this.myBody.appendChild(myHr);
   this.myBody.appendChild(myP);
   if (this.total % 2 == 1) {
-    myP.style='background:gray';
+    if (color == undefined)
+      myP.style='background:gray';
+    else
+      myP.style='background-color:' + color.toLowerCase();
   }
+}
+
+// 添加 "标签" 方法
+// 将标签 femaleTag 添加到 标签 subTag
+showValue.prototype.appendTag(femaleTag, subTag) {
+  femaleTag.appendChild(subTag);
+}
+
+// 添加 "文字" 方法
+// 将字段 text 变量,添加到标签 femaleTag 当中
+showValue.prototype.appendText(femaleTag, text) {
+  femaleTag.innerHTML += text.toString();
 }
