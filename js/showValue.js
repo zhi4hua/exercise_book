1e109ccc5fbe51b0363a4b11a542bbc45e0099ad
zhi4hua <zhi4hua@yeah.net>
2015-12-06 14:51:31 +0800

    第3次完善
    修正了不能开机的问题.

--- js/showValue.js
+++ js/showValue.js
@@ -2,48 +2,42 @@
 function showValue() {
   this.myBody = null;
   this.total = 0;
+  this.index = 1;
+  this.bgColor = 'gray';
 }
 
 // 类方法，功能为－追加变量致 Body 标签当中
-showValue.prototype.app = function(value1, color, index) {
+showValue.prototype.app = function(value1, color, index1) {
   this.myBody = document.getElementsByTagName('body')[0];
   var myHr = document.createElement('hr');
   var myP = document.createElement('p');
+  var text = this.index + '. ';
 
-  if (typeof(index) == "number")
-    this.total = index;
-  if (index == undefined)
-    myP.innerHTML = ++this.total + '&#9' + value1;
-  else if (index == false)
-    myP.innerHTML = value1;
-
-  this.myBody.appendChild(myHr);
-  this.myBody.appendChild(myP);
-  if (this.total % 2 == 1) {
-    if (color == undefined)
-      myP.style='background:gray';
-    else
-      myP.style='background-color:' + color.toLowerCase();
+  if (typeof(index) == 'number')
+  	this.index = parseInt(index);
+  else if (index1 == false)
+  	text = '';
+  if (this.total % 2 != 0)
+  	this.appendTag(this.myBody, myHr);
+  this.appendTag(this.myBody, myP);
+  if (this.index % 2 == 1) {
+    if (color != undefined)
+    	this.bgColor = color;
+    myP.style.backgroundColor = this.bgColor;
   }
+  myP.innerHTML = text + value1.toString();
+  ++ this.index;
+  ++ this.total;
 }
 
 // 添加 "标签" 方法
 // 将标签 femaleTag 添加到 标签 subTag
-showValue.prototype.appendTag(femaleTag, subTag) {
+showValue.prototype.appendTag = function(femaleTag, subTag) {
   femaleTag.appendChild(subTag);
 }
 
 // 添加 "文字" 方法
 // 将字段 text 变量,添加到标签 femaleTag 当中
-showValue.prototype.appendText(femaleTag, text) {
+showValue.prototype.appendText = function(femaleTag, text) {
   femaleTag.innerHTML += text.toString();
-<<<<<<< HEAD
 }
-=======
-}
-
-// 设置"母"标签
-showValue.prototype.setParentTag(tagName) {
-  this.myBody = tagName;
-}
->>>>>>> 0837676a2c16a20a78d5ef2f4582a7f6d6ee91a4
