5d858e821238fd50ce72b82683bccde0d7efddeb
zhi4hua <zhi4hua@yeah.net>
2016-01-05 09:58:22 +0800

    Update showValue.js

--- js/showValue.js
+++ js/showValue.js
@@ -56,16 +56,16 @@ showValue.prototype.appendText = function (text, femaleTag) {
 
 // 生成标签
 // 按参数 Tag 指定的类型生成标签
-// showValue.prototype.createTag = function (Type) {
-// 	var len = arguments.length;
-// 	alert("To this");
+showValue.prototype.createTag = function (Type) {
+	var len = arguments.length;
 	
-// 	if (len >= 0 && len <= 1) {
-// 		if len == 0
-// 			Type = 'h2';
-// 		return document.createElement(Type);
-// 	}
-// 	else
-// 		throw Error("Wrong number of parameters");
+	if (len >= 0 && len <= 1) {
+		if len == 0 {
+			Type = 'h2';
+		}
+		return document.createElement(Type);
+	}
+	else
+		throw Error("Wrong number of parameters");
 			
-// }
+}
